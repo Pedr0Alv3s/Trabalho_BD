b98ee5c5e2c38160e2b2d769ccc14a7886982e48
Pedro <170757825+Pedr0Alv3s@users.noreply.github.com>
2024-11-10 23:34:52 -0300

    Delete PROJETO directory

--- PROJETO/src/main/java/com/example/PROJETO/repository/Cargorepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.PROJETO.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.PROJETO.model.Cargo;
-
-public interface Cargorepository extends JpaRepository<Cargo, Integer> {
-	Optional<Cargo> findByNome(String crg_nome);
-}
